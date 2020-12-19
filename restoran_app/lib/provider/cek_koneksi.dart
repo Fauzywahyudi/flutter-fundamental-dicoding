141050dee2a5f5df5f9cd2d9d0a9c4d3d064df7b
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-12-20 00:29:16 +0700

    clean code, fix submission 2

--- restoran_app/lib/provider/cek_koneksi.dart
+++ restoran_app/lib/provider/cek_koneksi.dart
@@ -1,49 +1,9 @@
 import 'dart:async';
-
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
 
 class CheckConnection {
-  StreamSubscription<DataConnectionStatus> listener;
-  var internetStatus = "Unknown";
-  var contentmessage = "Unknown";
-
-  void _showDialog(String title, String content, BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: new Text('Connection Status'),
-          content: new Text(content),
-          actions: <Widget>[
-            new FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: new Text("Close"))
-          ],
-        );
-      },
-    );
-  }
-
   Future<DataConnection> checkConnection(BuildContext context) async {
-    // listener = DataConnectionChecker().onStatusChange.listen((status) {
-    //   switch (status) {
-    //     case DataConnectionStatus.connected:
-    //       internetStatus = "Connected to the Internet";
-    //       contentmessage = "You are connected to the Internet.";
-    //       isConnected = true;
-    //       // _showDialog(internetStatus, contentmessage, context);
-    //       break;
-    //     case DataConnectionStatus.disconnected:
-    //       internetStatus = "You are disconnected to the Internet. ";
-    //       contentmessage = "Please check your internet connection";
-    //       isConnected = false;
-    //       // _showDialog(internetStatus, contentmessage, context);
-    //       break;
-    //   }
-    // });
     bool isConnected;
     String message = '';
     final connection = await DataConnectionChecker().connectionStatus;
